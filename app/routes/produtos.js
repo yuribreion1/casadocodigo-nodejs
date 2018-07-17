58b0420e5eeba053adb571b0009b660b1c976726
Yuri Breion <yuribreion@gmail.com>
2018-07-16 22:54:06 -0300

    Adicionando tratamento de campos em branco

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,5 +1,8 @@
 module.exports = app => {
 
+    var connection = app.infra.connectionFactory();
+    var produtoDAO = new app.infra.ProdutoDAO(connection);
+
     app.get('/produtos', (req, res) => {
         console.log('Listando produtos');
 
@@ -26,17 +29,34 @@ module.exports = app => {
     });
 
     app.get('/produtos/form', (req, res) => {
-        res.render('produtos/form');
+        res.render('produtos/form', { errosValidacao: {}, produto: {} });
     });
 
     app.post('/produtos', (req, res) => {
 
-        var connection = app.infra.connectionFactory();
-        var produtoDAO = new app.infra.ProdutoDAO(connection);
-
         var produto = req.body;
+        console.log(produto);
+
+
+        req.assert('Nome do livro', 'Titulo é obrigatório').notEmpty();
+        req.assert('Descrição do livro', 'Descrição é obrigatória').notEmpty();
+
+        var erros = req.validationErrors();
+        if (erros) {
+            res.format({
+                html: () => {
+                    res.status(400).render('produtos/form', { errosValidacao: erros, produto: produto });
+                },
+                json: () => {
+                    res.status(400).json(erros);
+                }
+            });
+
+            return;
+        }
 
         produtoDAO.salva(produto, (err, data) => {
+
             if (!err) {
                 res.format({
                     html: () => {
