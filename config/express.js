bf4d789dae2ca3ddad9f34348bc4b9060fd97c62
Yuri Breion <yuribreion@gmail.com>
2018-07-16 22:53:21 -0300

    Alterando carregamento para uso do Express Validation

--- config/express.js
+++ config/express.js
@@ -2,6 +2,7 @@ var express = require('express');
 var load = require('express-load');
 var logger = require('morgan');
 var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
 
 module.exports = () => {
     var app = express();
@@ -11,6 +12,8 @@ module.exports = () => {
     app.set('views', './app/views');
     app.use(bodyParser.urlencoded({ extended: true }));
     app.use(bodyParser.json());
+    app.use(expressValidator());
+    
     load('infra', { cwd: 'app', verbose: true })
         .then('routes')
         .into(app);
